bab055990ca7542852da165a968e0af82880b280
Bharath.1o <frozenbmg@gmail.com>
2026-05-07 22:27:27 +0530

    model.py
    Updated RandomForestClassifier parameters to increase n_estimators and set max_depth.

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
-# ---------------- TRAIN MODEL ----------------
 def train_and_save():
 
     df = pd.read_csv("dataset.csv")
@@ -11,7 +10,8 @@ def train_and_save():
     y = df.iloc[:, -1]
 
     model = RandomForestClassifier(
-        n_estimators=300,
+        n_estimators=500,
+        max_depth=20,
         random_state=42
     )
 
@@ -20,10 +20,7 @@ def train_and_save():
     with open("model.pkl", "wb") as file:
         pickle.dump(model, file)
 
-# ---------------- LOAD MODEL ----------------
 def load_model():
 
     with open("model.pkl", "rb") as file:
-        model = pickle.load(file)
-
-    return model
+        return pickle.load(file)
