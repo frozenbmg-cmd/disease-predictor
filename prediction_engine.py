d0314a8a7bd92522c3f287879fc63caf627cd845
Bharath.1o <frozenbmg@gmail.com>
2026-05-11 13:01:26 +0530

    prediction_engine.py

--- prediction_engine.py
+++ prediction_engine.py
@@ -1,1 +1,37 @@
+from disease_database import DISEASES
 
+# ---------------- PREDICTION ----------------
+def predict_disease(features):
+
+    results = []
+
+    for disease, symptoms in DISEASES.items():
+
+        score = 0
+        total = 0
+
+        for symptom, weight in symptoms.items():
+
+            total += weight
+
+            if features.get(symptom):
+                score += weight
+
+        confidence = round(
+            (score / total) * 100,
+            2
+        )
+
+        if confidence > 0:
+
+            results.append({
+                "disease": disease,
+                "confidence": confidence
+            })
+
+    results.sort(
+        key=lambda x: x["confidence"],
+        reverse=True
+    )
+
+    return results[:3]
