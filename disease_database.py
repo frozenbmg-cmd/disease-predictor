9552682d2151468937eb38bdc401c1aa105a444f
Bharath.1o <frozenbmg@gmail.com>
2026-05-11 13:00:04 +0530

    disease_database.py

--- disease_database.py
+++ disease_database.py
@@ -1,1 +1,179 @@
 
+DISEASES = {
+
+    # ---------------- VIRAL ----------------
+
+    "Flu": {
+        "fever": 4,
+        "cough": 4,
+        "body pain": 3,
+        "fatigue": 3,
+        "headache": 2
+    },
+
+    "COVID-19": {
+        "fever": 4,
+        "cough": 4,
+        "loss of taste": 5,
+        "breathlessness": 4,
+        "fatigue": 3
+    },
+
+    "Dengue": {
+        "fever": 5,
+        "body pain": 5,
+        "headache": 3,
+        "chills": 3,
+        "weakness": 2
+    },
+
+    "Malaria": {
+        "fever": 5,
+        "chills": 5,
+        "sweating": 4,
+        "fatigue": 3,
+        "dizziness": 2
+    },
+
+    "Typhoid": {
+        "fever": 5,
+        "abdominal pain": 4,
+        "weakness": 3,
+        "headache": 2
+    },
+
+    "Chickenpox": {
+        "fever": 3,
+        "rash": 5,
+        "itching": 5
+    },
+
+    "Measles": {
+        "fever": 4,
+        "rash": 4,
+        "cough": 2
+    },
+
+    "Viral Fever": {
+        "fever": 4,
+        "fatigue": 3,
+        "headache": 2
+    },
+
+    # ---------------- RESPIRATORY ----------------
+
+    "Asthma": {
+        "breathlessness": 5,
+        "cough": 3,
+        "chest pain": 2
+    },
+
+    "Pneumonia": {
+        "fever": 4,
+        "cough": 4,
+        "breathlessness": 4,
+        "chest pain": 3
+    },
+
+    "Bronchitis": {
+        "cough": 5,
+        "chest pain": 3,
+        "fatigue": 2
+    },
+
+    "Tuberculosis": {
+        "cough": 5,
+        "fever": 3,
+        "weakness": 4,
+        "chest pain": 2
+    },
+
+    "Sinusitis": {
+        "headache": 4,
+        "congestion": 4,
+        "runny nose": 3
+    },
+
+    "Common Cold": {
+        "cough": 4,
+        "runny nose": 5,
+        "sneezing": 4,
+        "congestion": 3
+    },
+
+    # ---------------- DIGESTIVE ----------------
+
+    "Food Poisoning": {
+        "vomiting": 5,
+        "diarrhea": 5,
+        "stomach pain": 4,
+        "nausea": 3
+    },
+
+    "Gastritis": {
+        "acidity": 5,
+        "stomach pain": 4,
+        "bloating": 3
+    },
+
+    "Constipation": {
+        "constipation": 5,
+        "abdominal pain": 3,
+        "bloating": 2
+    },
+
+    "IBS": {
+        "diarrhea": 4,
+        "bloating": 4,
+        "stomach pain": 4
+    },
+
+    # ---------------- NEUROLOGICAL ----------------
+
+    "Migraine": {
+        "headache": 5,
+        "dizziness": 3,
+        "nausea": 3
+    },
+
+    "Vertigo": {
+        "dizziness": 5,
+        "nausea": 2
+    },
+
+    # ---------------- SKIN ----------------
+
+    "Allergy": {
+        "rash": 4,
+        "itching": 5,
+        "sneezing": 2
+    },
+
+    "Eczema": {
+        "rash": 5,
+        "itching": 5
+    },
+
+    "Fungal Infection": {
+        "itching": 5,
+        "rash": 4
+    },
+
+    # ---------------- LIFESTYLE ----------------
+
+    "Diabetes": {
+        "fatigue": 3,
+        "weakness": 4
+    },
+
+    "Dehydration": {
+        "dizziness": 4,
+        "weakness": 4,
+        "fatigue": 3
+    },
+
+    "Hypertension": {
+        "headache": 3,
+        "dizziness": 3
+    }
+}
