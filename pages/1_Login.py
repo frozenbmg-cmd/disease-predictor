f99034cc639133f7a5c2b40d720db9627bbba7d7
Bharath.1o <frozenbmg@gmail.com>
2026-05-11 12:57:09 +0530

    1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,1 +1,47 @@
 
+import streamlit as st
+from auth import login
+
+st.set_page_config(page_title="Login")
+
+# ---------------- CSS ----------------
+st.markdown("""
+<style>
+
+.stApp {
+    background-color: #050816;
+    color: white;
+}
+
+.login-box {
+    padding: 30px;
+    border-radius: 20px;
+    background-color: #121a2b;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+# ---------------- TITLE ----------------
+st.title("🔐 Login")
+
+st.markdown("### AI Disease Prediction System")
+
+username = st.text_input("Username")
+
+password = st.text_input(
+    "Password",
+    type="password"
+)
+
+if st.button("Login"):
+
+    if login(username, password):
+
+        st.session_state.logged_in = True
+        st.session_state.username = username
+
+        st.success("Login Successful")
+
+    else:
+        st.error("Invalid Username or Password")
