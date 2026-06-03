1e5ccd0cfde597de85214fbfcc260e1e07cb738d
Bharath.1o <frozenbmg@gmail.com>
2026-06-03 21:09:01 +0530

    .

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import streamlit as st
 
-# ---------------- PAGE CONFIG ----------------
-
 st.set_page_config(
-page_title="AI Health Assistant",
-page_icon="🩺",
-layout="wide"
+    page_title="AI Health Assistant",
+    page_icon="🩺",
+    layout="wide"
 )
-
-# ---------------- SESSION ----------------
-
-if "page" not in st.session_state:
-st.session_state.page = "register"
-
-if "logged_in" not in st.session_state:
-st.session_state.logged_in = False
 
 st.title("🩺 AI Health Assistant")
 
-st.success("App Loaded Successfully")
+st.success("Application Loaded Successfully")
